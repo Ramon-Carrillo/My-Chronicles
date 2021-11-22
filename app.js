c80bdd505a236ab06a0f6d92d06a539ee2a2618d
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-11-22 15:18:08 -0600

    adjusted port

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ app.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('auth_practice running on port 3000');
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env,
+  );
 });
